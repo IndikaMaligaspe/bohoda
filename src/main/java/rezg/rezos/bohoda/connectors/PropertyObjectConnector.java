03f9630f2557605da392341a3cdf4f3dbda1cd63
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2016-11-12 07:41:36 +0530

    Added Testes for PropertyFile generation

---
+++ src/main/java/rezg/rezos/bohoda/connectors/PropertyObjectConnector.java
@@ -0,0 +1,92 @@
+package rezg.rezos.bohoda.connectors;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+
+import net.minidev.json.JSONArray;
+
+public class PropertyObjectConnector {
+
+	private String bohodaService;
+	private URL mURL;
+	private HttpURLConnection uConn;
+	private DataInputStream dis;
+	private String serviceResponse;
+	private Logger logger = (Logger) Logger.getInstance(PropertyObjectConnector.class);
+
+	public Object handleRequest(String file, String project, String environment) {
+		Properties props = null;
+		try {
+			props = new Properties();
+
+		} catch (Exception e) {
+			props = null;
+		}
+		// TODO Auto-generated method stub
+		return props;
+	}
+
+	public int connect(String bohodaService) {
+		int responseCode = 500;
+		try {
+			logger.info(bohodaService);
+			mURL = new URL(bohodaService);
+			uConn = (HttpURLConnection) mURL.openConnection();
+			uConn.setDoInput(true);
+			uConn.setDoOutput(true);
+			responseCode = uConn.getResponseCode();
+			dis = new DataInputStream(uConn.getInputStream());
+			StringBuffer sbr = new StringBuffer();
+			String line = dis.readLine();
+			sbr.append(line);
+			while (null != (line = dis.readLine())) {
+				line = dis.readLine();
+				sbr.append(line);
+			}
+			this.serviceResponse = sbr.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseCode = 500;
+		}
+
+		return responseCode;
+	}
+
+	public Properties convertServicResponse(String JSONText) throws IllegalArgumentException, JsonParseException, JsonMappingException, IOException{
+		Properties prop = new Properties();
+		Map <String,String>propertyMap = new HashMap<String,String>();
+		
+			JSONArray array = JsonPath.parse(JSONText).read("$.propertySources..source");
+			
+			if(array.isEmpty())
+				throw new IllegalArgumentException("Source List missing from property file!!!");
+			if(array.size()==0)
+				throw new IllegalArgumentException("Source List size is 0 for property file!!!");
+			if((array.size()==1) && (null==array.get(0)))
+				throw new IllegalArgumentException("Source is NULL !!!");
+			
+			String propertyString = new String(array.toJSONString());
+			logger.debug("Converted JSON String *************** " + propertyString);
+			propertyString = propertyString.substring(1, propertyString.length() - 1);
+			propertyMap = new ObjectMapper().readValue( propertyString,
+					new com.fasterxml.jackson.core.type.TypeReference<Map<String,String>>() {
+					});
+			prop.putAll(propertyMap);
+			propertyMap.clear();
+		
+		return prop;
+	}
+
+}
---
+++ src/test/java/rezg/rezos/bohoda/connectors/PropertyObjectConnectorTest.java
@@ -0,0 +1,89 @@
+package rezg.rezos.bohoda.connectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.Properties;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@PropertySource("classpath:bootstrap.yml")
+public class PropertyObjectConnectorTest {
+
+	private PropertyObjectConnector target = new PropertyObjectConnector();
+	private String file;
+	private String project;
+	private String environment;
+
+	
+	// @Value("${bohoda.server.uri}")
+	private String bohodaService = "http://localhost:8888";
+	
+	@Test
+	@Ignore
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testHandleRequest(){
+		file = "oman_config_portals";
+		project = "development";
+		environment = "master";
+		Object response = target.handleRequest(file,project,environment);
+		assertNotNull(response);
+	}
+	
+	@Test
+	public void testConnect(){
+		file = "oman_config_portals";
+		project = "development";
+		environment = "master";
+		int response = target
+				.connect(bohodaService + "?file=" + file + "&project=" + project + "&environment=" + environment);
+		assertNotNull(response);
+		assertEquals(200, response);
+	}
+	
+	@Test
+	public void testConvertToPropertiesObject() throws Exception{
+		String JSONString = "{\"name\":\"configclient\",\"profiles\":[\"et\"],\"label\""
+				+ ":\"qa\",\"version\":\"39f68bd3a9839e3cdd26053e41ad35a1fea2519a\",\"state\":null,\""
+				+ "propertySources\":[{\"name\":\"https://github.com/IndikaMaligaspe/bohoda-config-test/configclient.yml\","
+				+ "\"source\":{\"configuration.projectName\":\"configclient\",\"server.port\":8000,\"message\":\""
+				+ "\",\"greeting\":\"Hello from the configuration in QA Environment!\"}}]}";
+		String source = "[{\"configuration.projectName\":\"configclient\",\"server.port\":8000,\"message\":\""
+				+ "\",\"greeting\":\"Hello from the configuration in QA Environment!\"}]";
+		Properties response = target.convertServicResponse(JSONString);
+		assertNotNull(response);
+		assertEquals("configclient", response.get("configuration.projectName").toString());
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConvertToPropertyObjectIfSourceNull() throws Exception{
+		String JSONString = "{\"name\":\"configclient\",\"profiles\":[\"et\"],\"label\""
+				+ ":\"qa\",\"version\":\"39f68bd3a9839e3cdd26053e41ad35a1fea2519a\",\"state\":null,\""
+				+ "propertySources\":[{\"name\":\"https://github.com/IndikaMaligaspe/bohoda-config-test/configclient.yml\","
+				+ "\"source\":null}]}";
+		Properties response = target.convertServicResponse(JSONString);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConvertToPropertyObjectIfPropertySourceEmpty() throws Exception{
+		String JSONString = "{\"name\":\"configclient\",\"profiles\":[\"et\"],\"label\""
+				+ ":\"qa\",\"version\":\"39f68bd3a9839e3cdd26053e41ad35a1fea2519a\",\"state\":null,\""
+				+ "propertySources\":[]}";
+		Properties response = target.convertServicResponse(JSONString);
+		assertNotNull(response);
+	}
+
+}
